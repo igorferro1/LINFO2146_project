67b81c05d7e245be8159e0b7ab94d74255405a6c
Igor Ferro <igorferro@usp.br>
2022-05-09 11:49:58 +0200

    progress on sensor nodes

--- sensor_node.c
+++ sensor_node.c
@@ -1,6 +1,7 @@
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
+#include "cc2420.h" // signal strength
 
 #include <string.h>
 #include <stdio.h> /* For printf() */
@@ -11,11 +12,18 @@ AUTOSTART_PROCESSES(&nullnet_example_process);
 /**
  * @brief Template of message that will be sent/received by the node
  */
+
+static int parent_addr[2];
+static int rank = 1000;
+static int rssi_parent = -1000;
+
 struct message
 {
     int mType;      /* data, find parents, etc */
     int broadOrUni; /* broadcast or unicast */
     int rankSender;
+    int addr_sender[2];
+    int addr_dest_opening[2]; /* to open valve we need the destination address */
     char data[32];
 };
 
@@ -26,17 +34,13 @@ struct message
 struct routingEntry
 {
     int target;
-    int nextJump;
+    int nextJump[2];
 };
 
 static struct routingEntry routingTable[64]; // static = init to 0 for all value
 static int route_table_len = sizeof(route_table) / sizeof(route_table[0]);
 
 struct message rcv_msg;
-/* idk if those are necessary
-struct message broad_rcv;
-struct message uni_rcv;
-*/
 
 /*---------------------------------------------------------------------------
  * Function that will be called when the node receives any data
@@ -52,18 +56,77 @@ void input_callback(const void *data, uint16_t len,
         print("Got message in node x (will be replaced with the rank)")
 
             if (rcv_msg.broadOrUni == 0)
-                rcv_broadcast(rcv_msg);
-        else rcv_unicast(rcv_msg);
+                rcv_broadcast(rcv_msg, &src);
+        else rcv_unicast(rcv_msg, &src);
     }
 }
 
-void rcv_broadcast(struct message rcv_msg)
+/* Types of messages to deal with:
+    - Sending broadcasts: advertise/down
+    - Receiving broadcasts: from advertisements/down
+    - Receiving unicasts/runicasts: from nodes forwarding data (par -> chi or chi -> par), or command to that specific node
+    - Sending unicasts/runicasts: to parents for data
+
+*/
+
+/* General logic for the sensor node to send data
+    - Initialize, find a parent
+    - wait one minute
+    - send random value to parent
+
+ */
+
+void rcv_broadcast(struct message rcv_msg, const linkaddr_t *src)
 {
-    /*  */
+    /* if it's a message of "i can be your parent"
+       possible scenarios:
+        - the node that sent the broadcast has a lower rank as the current parent: compare rssis and change parents if the new is better
+        - the node that sent the broadcast has the same rank as the current parent: compare rssis and change parent if the new is better
+        - the node that sent the broadcast has a higher rank: keep parent
+        */
+    if (rcv_msg.mType == 0 && rcv_msg.rankSender < rank) /* "i can be your parent" message */
+        if (rcv_msg.rankSender <= rank - 1)
+        {
+            if ((src->u8[0] != parent_addr[0] || src->u8[1] != parent_addr[1]) && rssi_parent > cc2420_last_rssi)
+            { /* if it's better, it assumes the position of parent */
+                parent_addr[0] = src->u8[0];
+                parent_addr[1] = src->u8[1];
+                rssi_parent = cc2420_last_rssi;
+                rank = rcv_msg.rankSender + 1;
+                /* advertise to next ones maybe? */
+            }
+            /* if it's the same parent, advertise to the next ones maybe? */
+        }
+    /* if it's a message of "i'm down!" and the node down is the parent of the node
+        - we have to reset the parameters and try to find another parent*/
+    if (rcv_msg.mType == 1 && parent_addr[0] == src->u8[0] && parent_addr[1] == src->u8[1])
+    {
+        rank = 1000;
+        parent_addr[0] = 0;
+        parent_addr[1] = 1;
+        rssi_parent = -1000;
+        /* tell other nodes about it and try to find a parent */
+        /* clean routing table maybe */
+    }
 }
 
-void rcv_unicast(struct message rcv_msg)
+void rcv_unicast(struct message rcv_msg, const linkaddr_t *src)
 {
+    /* if it's data to send to the server:
+        - we could add the sending node to the routing table, connecting by the data send
+    */
+    if (rcv_msg.mType == 2)
+    {
+        /* add to routing table and forward it upwards? */
+    }
+
+    /* if it's just an open valve message:
+        - check if it's for this node
+            - if it's for this node, then open
+            - otherwise look in the routing table and send the message to the next node */
+    else if (rcv_msg.mType == 3)
+    {
+    }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(nullnet_example_process, ev, data)
@@ -112,18 +175,3 @@ int randomValue()
 /* TODO: See how to do the routing from scratch (follow the examples that they gave and check the TPs again)
    At first I thought we could use Rime, but apparently not
 */
-
-/* Types of messages to deal with:
-    - Sending broadcasts: find parent
-    - Receiving broadcasts: from nodes searching for parents
-    - Receiving unicasts/runicasts: from nodes forwarding data (par -> chi or chi -> par), or command to that specific node
-    - Sending unicasts/runicasts: to parents for data
-
-*/
-
-/* General logic for the sensor node to send data
-    - Initialize, find a parent
-    - wait one minute
-    - send random value to parent
-
- */
